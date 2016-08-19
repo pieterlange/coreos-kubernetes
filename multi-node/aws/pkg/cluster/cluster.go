b19680eef6a1d3759ac0f14d6633dac0602ab1ca
Pieter Lange <pieter@ptlc.nl>
2016-08-19 15:44:49 +0200

    Some hacks to allow spinning up a discrete etcd cluster outside of kube-aws tooling. Also allow toggling of public IP's.

--- multi-node/aws/pkg/cluster/cluster.go
+++ multi-node/aws/pkg/cluster/cluster.go
@@ -223,24 +223,34 @@ func (c *Cluster) createStack(cfSvc cloudformationService, stackBody string) (*c
 		value := v
 		tags = append(tags, &cloudformation.Tag{Key: &key, Value: &value})
 	}
-
-	creq := &cloudformation.CreateStackInput{
-		StackName:    aws.String(c.ClusterName),
-		OnFailure:    aws.String(cloudformation.OnFailureDoNothing),
-		Capabilities: []*string{aws.String(cloudformation.CapabilityCapabilityIam)},
-		TemplateBody: &stackBody,
-		Tags:         tags,
-		StackPolicyBody: aws.String(`{
-  "Statement" : [
-    {
-      "Effect" : "Deny",
-      "Action" : "Update:*",
-      "Principal" : "*",
-      "Resource" : "LogicalResourceId/InstanceEtcd*"
-    }
-  ]
-}
-`),
+	var creq *cloudformation.CreateStackInput
+	if(c.EtcdLoadBalancer != "") {
+		creq = &cloudformation.CreateStackInput{
+			StackName:    aws.String(c.ClusterName),
+			OnFailure:    aws.String(cloudformation.OnFailureDoNothing),
+			Capabilities: []*string{aws.String(cloudformation.CapabilityCapabilityIam)},
+			TemplateBody: &stackBody,
+			Tags:         tags,
+			StackPolicyBody: aws.String(`{
+		"Statement" : [
+			{
+				"Effect" : "Deny",
+				"Action" : "Update:*",
+				"Principal" : "*",
+				"Resource" : "LogicalResourceId/InstanceEtcd*"
+			}
+		]
+	}
+	`),
+		}
+	} else {
+		creq = &cloudformation.CreateStackInput{
+			StackName:    aws.String(c.ClusterName),
+			OnFailure:    aws.String(cloudformation.OnFailureDoNothing),
+			Capabilities: []*string{aws.String(cloudformation.CapabilityCapabilityIam)},
+			TemplateBody: &stackBody,
+			Tags:         tags,
+		}
 	}
 
 	return cfSvc.CreateStack(creq)
@@ -317,9 +327,11 @@ func (c *Cluster) lockEtcdResources(cfSvc *cloudformation.CloudFormation, stackB
 func (c *Cluster) Update(stackBody string) (string, error) {
 
 	cfSvc := cloudformation.New(c.session)
-	var err error
-	if stackBody, err = c.lockEtcdResources(cfSvc, stackBody); err != nil {
-		return "", err
+	if(c.EtcdLoadBalancer != "") {
+		var err error
+		if stackBody, err = c.lockEtcdResources(cfSvc, stackBody); err != nil {
+			return "", err
+		}
 	}
 	input := &cloudformation.UpdateStackInput{
 		Capabilities: []*string{aws.String(cloudformation.CapabilityCapabilityIam)},
